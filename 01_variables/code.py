c063ab6802d5138639e5bc7cf6a24dc71e1826aa
Daniele Puccio <daniele.puccio@yahoo.com>
2022-10-23 19:46:46 +0200

    variables in python

---
+++ 01_variables/code.py
@@ -0,0 +1,28 @@
+# -- Numbers and Floats --
+from re import A
+
+
+x = 15
+price = 9.99
+discount = 0.2
+result = price*(1-discount)
+print(result)
+
+# -- Strings --
+name = "Rolf"
+print(name)
+print(name*2)
+print(name+name)
+
+# -- Changing variables --
+
+a=25
+b=a
+print(a)
+print(b)
+
+b=17
+
+print(a)
+print(b)
+
