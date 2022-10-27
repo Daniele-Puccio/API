38d0db6752bc9f88e9fa1f3ce704098e57287367
Daniele-Puccio <daniele.puccio@yahoo.com>
2022-10-27 18:30:59 +0200

    OOP

---
+++ 23_object_oriented_programming/code.py
@@ -0,0 +1,11 @@
+Class Student:
+    def __init__(self,name,grades):
+        self.name = name
+        self.grades = grades
+
+    def average_grade(self):
+        return sum(self.grades)/len(self.grades)
+
+
+student=Student("Bob",(25,60,39))
+print(student.average_grade())
