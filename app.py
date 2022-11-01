fa1cb764073340669bf72fd8cfa525dd3aabf810
Daniele Puccio <daniele.puccio@yahoo.com>
2022-11-01 18:18:50 +0100

    user register endpoint

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import models
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
 
 
 def create_app(db_url="sqlite:///data.db"):
@@ -34,4 +35,5 @@ def create_app(db_url="sqlite:///data.db"):
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
     api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
     return app
