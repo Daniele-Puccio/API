84aa62f4aeaf0256edc615f5441a7bc260333ef6
Daniele Puccio <daniele.puccio@yahoo.com>
2022-11-01 10:36:15 +0100

    get by id or retrurn a 404

--- resources/store.py
+++ resources/store.py
@@ -1,6 +1,9 @@
 import uuid
 from flask import request
 from flask.views import MethodView
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
+from db import db
+from models import StoreModel
 from flask_smorest import Blueprint, abort
 from schema import StoreSchema
 
@@ -10,30 +13,16 @@ blp = Blueprint("stores", __name__, description="Operations on stores")
 class Store(MethodView):
     @blp.response(200, StoreSchema)
     def get(self, store_id):
-        try:
-            return stores[store_id]
-        except KeyError:
-            abort(404, message="Store not found!")
+        store = StoreModel.query.get_or_404(store_id)
+        return store
 
     def delete(self, store_id):
-        try:
-            del stores[store_id]
-            return {"message": "Store deleted."}
-        except KeyError():
-            abort(404, message="Store not found!")
+        store = StoreModel.query.get_or_404(store_id)
+        raise NotImplementedError("Deleting a store is not implemented.")
+
     def put(self, store_id):
-        store_data=request.get_json()
-        if (
-            "name" not in store_data or
-            store_data['name'] == ""
-        ):
-            abort(499, message="Bad request. Ensure 'name' are included correctly in the JSON payload.")
-        try:
-            store=stores[store_id]
-            store |= store_data
-            return store
-        except KeyError:
-            abort(404, message="Store not found.")
+        store = StoreModel.query.get_or_404(store_id)
+        raise NotImplementedError("Updating a store is not implemented.")
 
 @blp.route("/store")
 class Store(MethodView):
@@ -43,12 +32,12 @@ class Store(MethodView):
     @blp.response(201,StoreSchema)
     @blp.arguments(StoreSchema)
     def post(self,store_data):
-        for store in stores.values():
-            if(store['name']==store_data['name']):
-                abort(400,message="Store already exists.")
-            if(store_data['name'] == ""):
-                abort(400,message="Store must have a name.")
-        store_id = uuid.uuid4().hex
-        store = {**store_data, "id":store_id}
-        stores[store_id] = store
-        return store, 201
+       store=StoreModel(**store_data)
+       try:
+        db.session.add(store)
+        db.session.commit()
+       except IntegrityError:
+        abort(400,message="A store with that name already exists.")
+       except SQLAlchemyError:
+        abort(500,message="AN error occurred creating the store.")
+       return store, 201 
