84de14fa70f6862023263e9b99bbff0ea87bd8dd
Daniele Puccio <daniele.puccio@yahoo.com>
2022-11-03 08:15:25 +0100

    adding settings.py for worker to render.com

--- tasks.py
+++ tasks.py
@@ -1,21 +1,31 @@
 import os
 import requests
+import jinja2
 from dotenv import load_dotenv
 
 load_dotenv()
 
 DOMAIN = os.getenv("MAILGUN_DOMAIN")
+template_loader=jinja2.FileSystemLoader("templates")
+template_env = jinja2.Environment(loader=template_loader)
 
-def send_simple_message(to, subject, body):
+def render_template(template_filename, **context):
+	return template_env.get_template(template_filename).render(**context)
+
+def send_simple_message(to, subject, body, html):
 	return requests.post(
 		f"https://api.mailgun.net/v3/{domain}/messages",
 		auth=("api", os.getenv("MAILGUN_API_KEY")),
 		data={"from": "Daniele Puccio <mailgun@{domain}>",
 			"to": [to],
 			"subject": subject,
-			"text": body})
+			"text": body,
+			"html": html
+		}
+	)
 def send_user_registration_email(email,username):
     return send_simple_message(email,
         "Successfully signed up",
-        f"Hi {user.username}!, You have successfully signed up to the Stores REST API."
+        f"Hi {user.username}!, You have successfully signed up to the Stores REST API.",
+		render_template("email/action.html")
         )
